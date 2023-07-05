5ad02b30c29db10b2574a47dc06064ca587e9e47
RedConcrete <franzbrunohubertklose@gmail.com>
2023-07-05 10:04:40 +0200

    desktop fast fertig jetzt mobile

--- script.js
+++ script.js
@@ -58,8 +58,12 @@ function addToCart() {
     calcCost();
 }
 
-function openCart() {
-    window.location.href = "cart.html";
+function checkout(){
+    console.log("Checkout");
+}
+
+function onDeleteButton(){
+    console.log("Delete");
 }
 
 function changeImage(image) {
@@ -133,17 +137,12 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
 
 function openModal(n) {
